6d8eb4016ba89a21270ef0df329c750ed0a8e396
ISMAIL <139564142+isacks456@users.noreply.github.com>
2024-12-16 22:53:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const count = document.getElementById('count');
 const circle = document.getElementById('main-btn');
 const levelUp = document.getElementById('levelUp');
 const boost = document.getElementById('boost');
+const btn = document.getElementById('btn');
+const profile = document.getElementById('profile');
+const scoreProfile = document.getElementById('scoreProfile');
+const levelUp1Subtitle = document.getElementById('levelUp-sub_title');
+const close = document.getElementById('close');
 let counter = 0;
 
 circle.addEventListener('click', () => {
@@ -19,5 +24,29 @@ circle.addEventListener('click', () => {
         levelUp.textContent = 'GOLD'
         levelUp.style.color = 'gold'
     };
+
+    if (counter === 10) {
+        levelUp1Subtitle.textContent = 'SILVER';
+        levelUp1Subtitle.style.color  = 'silver'
+    };
+
+    if (counter === 20) {
+        levelUp1Subtitle.textContent = 'GOLD'
+        levelUp1Subtitle.style.color = 'gold'
+    };
+
+
 });
+
+btn.addEventListener('click', () => {
+    profile.style.display = 'block';
+});
+
+circle.addEventListener('click', () => {
+    scoreProfile.textContent = counter;
+});
+
+close.addEventListener('click', () => {
+    profile.style.display = 'none';
+})
 
